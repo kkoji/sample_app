11983b48d6a4ef3b18914762e05c635618328d98
Kazuya Kojima <ypcjh662@yahoo.co.jp>
2015-02-19 16:46:34 +0900

    Finish sign in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,16 +44,14 @@ Spork.prefork do
     config.order = "random"
 
     config.include Capybara::DSL
+
+    config.before do
+      FactoryGirl.reload
+    end
   end
 end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
 
-end
-
-RSpec.configure do |config|
-  config.before do
-    FactoryGirl.reload
-  end
 end
